3d45fe450d96003a6723b2e82dfeddf6f9f6a702
Gabriel Martins <correio.gab@gmail.com>
2025-09-03 18:34:44 +0100

    ci(script): add a script to build and release locally

---
+++ release-local.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+version="v0.1.0"
+
+LATEST_TAG=$(git tag --sort=-v:refname | head -n 1)
+
+echo "Releasing jiralog-${LATEST_TAG} locally..."
+
+go build -o build/jiralog-${LATEST_TAG} main.go
+
+sudo cp ./build/jiralog-${LATEST_TAG} /usr/local/bin/jiralog
+
+echo "Done."
+
+exit 0
